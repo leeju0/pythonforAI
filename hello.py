36af7733bdf57905b7b7b41aea3f0d3d16e71365
leeju0 <92801522+leeju0@users.noreply.github.com>
2024-01-12 03:06:11 +0900

    tensorflow 설치 완

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print(1)
+import tensorflow as tf
+
+with tf.compat.v1.Session() as sess:
+  h = tf.constant("Hello")
+  w = tf.constant("World!")
+  hw = h + w
+  ans = sess.run(hw)
+  print(ans)
+
