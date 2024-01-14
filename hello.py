61c468bdec978d3c734ea5bca473fafc1636472c
leeju0 <92801522+leeju0@users.noreply.github.com>
2024-01-14 23:04:25 +0900

    조건문

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-x = int(input("Enter an integer"))
+weight = int(input('Enter weight: '))
+height = int(input('Enter height: '))
 
-if x == 1:
-    print("One")
+if weight >= 90 and height <= 150:
+    print('fat!', 'exercise!')
+elif weight >= 60 or 160 <= height <180:
+    print('normal')
 else:
-    print("Not one")
-
+    print('overweight')
