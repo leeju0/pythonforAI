af500b7214fc5e767428c6efcfc06fc721706086
leeju0 <92801522+leeju0@users.noreply.github.com>
2024-01-15 00:01:09 +0900

    map, split()

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-weight = int(input('Enter weight: '))
-height = int(input('Enter height: '))
+A,B,C = map(int,input().split()) #여러 값을 한번에 입력받는 방법. m
+"""
+    map(매칭할자료형,입력받을값) 
+    split() : 입력값을 받아서 하나씩 쪼개서 iteration형태로 만듦
+    sep = '\n' 줄바꿈으로 구분하겠음.
+"""
 
-if weight >= 90 and height <= 150:
-    print('fat!', 'exercise!')
-elif weight >= 60 or 160 <= height <180:
-    print('normal')
-else:
-    print('overweight')
+
+print(((A+B)%C),((A%C) + (B%C))%C,(A*B)%C,((A%C) * (B%C))%C, sep='\n')
